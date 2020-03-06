540861b7e6f02a0badaa3d72697ff92279699555
Yan <yan.mezhov@yandex.ru>
2020-03-07 01:28:18 +0300

    new

--- Assets/Scripts/PanelsUI/InformationPanel/SubPanel/EconomicPanel.cs
+++ Assets/Scripts/PanelsUI/InformationPanel/SubPanel/EconomicPanel.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EconomicPanel : MonoBehaviour
 {
     private State state;
+
     public void ShowEconomic(State state)
     {
         this.state = state;
@@ -14,29 +16,44 @@ public class EconomicPanel : MonoBehaviour
     private void UpdateInformation()
     {
         //use it
-        //state.regions[i].data.CalculateIncome();
+        //state.regions[i].data.CalculateIncome(); - Доход провинции 
         //state.army[i].GetUpkeep();
     }
     private void OnDisable()
     {
         state.IncomeChanges -= UpdateInformation;
     }
-    void DFS(List<Vector2Int> path, Vector2Int cur, Vector2Int? prev)
+    
+}
+
+class Block:MonoBehaviour
+{
+    [SerializeField] private int[] Color = new int[4];
+    public static void Generate(Vector2Int pos, Block[] prefabs)
     {
-        path.Add(cur);
-        Vector2Int[] delta = new Vector2Int[] { new Vector2Int(0, 1), new Vector2Int(0, -1), new Vector2Int(1, 0), new Vector2Int(-1, 0) };
-        bool IsEdge(Vector2Int a, Vector2Int b)
+        /*int n = 100, k = 0;
+        Queue<Vector2Int> q = new Queue<Vector2Int>();
+        Dictionary<Vector3Int, Block> dictionary = new Dictionary<Vector3Int, Block>();
+        Vector2Int[] dxy = new Vector2Int[4] { new Vector2Int(1, 0), new Vector2Int(-1, 0), new Vector2Int(0, 1), new Vector2Int(0, -1) };
+        
+        q.Enqueue(pos);
+        while (q.Count > 0 && k++ < n)
         {
-            return 1 < 10;
-        }
-        foreach(var d in delta)
-        {
-            Vector2Int next = cur + d;
-            if(next != prev && IsEdge(cur, next))
+            Vector2Int p = q.Dequeue();
+            int[] conditional = new int[4]; 
+            for (int i = 0; i < dxy.Length; i++)
             {
-                DFS(path, next, cur);
-                break;
+                Vector2Int t = p + dxy[i];
+                Block block;
+                if(dictionary.TryGetValue(t, out block))
+                {
+                    conditional[i] = block.Color[i];
+                }
             }
+           // Block prefab = GetBlock(conditional, prefabs);
+            //Instantiate(prefab, new Vector3(p.x, p.y, 0));
         }
+        */
     }
 }
+
